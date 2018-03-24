c3a512e5dfd3cba63145995ebaec9da85fe5edf6
InclementKing <37747224+InclementKing@users.noreply.github.com>
2018-03-24 14:46:06 -0500

    Update baseInstall.sh

--- baseInstall.sh
+++ baseInstall.sh
@@ -5,5 +5,7 @@ mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
 pacstrap /mnt base base-devel
 genfstab -L /mnt >> /mnt/etc/fstab
+wget raw.githubusercontent.com/InclementKing/Arch-setup/master/chrooted.sh
+mv chrooted.sh /mnt
 echo
 echo Now run arch-chroot /mnt, and run chrooted
