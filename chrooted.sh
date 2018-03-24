692b651b8f7c7f2456e32406f6565fc0cd66f43e
InclementKing <37747224+InclementKing@users.noreply.github.com>
2018-03-24 14:18:57 -0500

    Add files via upload

---
+++ chrooted.sh
@@ -0,0 +1,13 @@
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+hwclock --systohc
+echo en_US.UTF-8\ UTF-8 >> /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo Desktop > /etc/hostname
+echo 127.0.1.1\	Desktop.localdomain\	Desktop >> /etc/hosts
+systemctl enable dhcpcd.service
+passwd
+pacman -S grub intel-ucode
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
