51951b78ae33f6aba850729e0455e565949fb51c
InclementKing <brohrsauce@gmail.com>
2018-03-25 14:43:00 -0500

    t

--- baseInstall.sh
+++ baseInstall.sh
@@ -3,7 +3,7 @@ parted /dev/sda mklabel msdos
 parted -a optimal /dev/sda mkpart primary 0% 100%
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
-pacstrap /mnt base base-devel grub intel-ucode git wget
+pacstrap /mnt base base-devel grub intel-ucode git
 genfstab -L /mnt >> /mnt/etc/fstab
 wget raw.githubusercontent.com/InclementKing/Arch-setup/master/chrooted.sh
 wget raw.githubusercontent.com/InclementKing/Arch-setup/master/systemSetup.sh
