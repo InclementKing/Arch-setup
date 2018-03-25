4a99cb5083e6de0ede994d48effbfd02bd797092
InclementKing <brohrsauce@gmail.com>
2018-03-25 17:35:43 -0500

    Edits

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Arch-setup-basics
 
 Pre install:
-  1. run 'wget bit.ly/archbase'
+  1. Run 'wget bit.ly/archbase'
+  2. Run 'zsh archbase'
+  3. when prompted, run chrooted.sh
 
 Post install:
   1. Run 'bash systemSetup.sh'
--- systemSetup.sh
+++ systemSetup.sh
@@ -12,4 +12,4 @@ makepkg -si
 cd ~/
 rm -rf ~/git/
 
-yaourt --noconfirm -s google-chrome spotify pkgtools pulseaudio-ctl pulseaudio-equalizer-ladspa
+yaourt --noconfirm -S google-chrome spotify pkgtools pulseaudio-ctl pulseaudio-equalizer-ladspa
