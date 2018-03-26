bb87dfe7b52f18ed1cf4cfc4d6d10f18828607f0
InclementKing <brohrsauce@gmail.com>
2018-03-26 08:14:26 -0500

    edits

--- baseInstall.sh
+++ baseInstall.sh
@@ -6,7 +6,9 @@ mount /dev/sda1 /mnt
 pacstrap /mnt base base-devel grub intel-ucode git xorg i3 nvidia deluge mosh openssh pavucontrol scrot ffmpeg virtualbox vlc xterm alsa-utils jre8-openjdk pulseaudio pulseaudio-alsa python python2 xorg-xinit dmenu
 genfstab -L /mnt >> /mnt/etc/fstab
 wget raw.githubusercontent.com/InclementKing/Arch-setup/master/chrooted.sh
+wget raw.githubusercontent.com/InclementKing/Arch-setup/master/chrooted.py
 mv chrooted.sh /mnt
+mv chrooted.py /mnt
 echo base install successful- now run python chrooted.py
 arch-chroot /mnt
 echo 
--- chrooted.py
+++ chrooted.py
@@ -5,8 +5,8 @@ username = input("username: ")
 userpw = input('User pw: ')
 
 stuff = open("stuff.txt", 'w')
-stuff.write("$ROOTPW=" + rootpw)
-stuff.write("$USER=" + username)
-stuff.write('PASS=' + userpw)
+stuff.write("$ROOTPW=" + rootpw + "\n")
+stuff.write("$USER=" + username + "\n")
+stuff.write('PASS=' + userpw + "\n")
 
 subprocess.call('chrooted.sh', shell=True)
