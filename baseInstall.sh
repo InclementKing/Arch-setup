82a1d55fde08c79114f97e083deac9c4ac66c6a1
InclementKing <brohrsauce@gmail.com>
2018-03-27 08:41:43 -0500

    edits

--- baseInstall.sh
+++ baseInstall.sh
@@ -6,9 +6,7 @@ mount /dev/sda1 /mnt
 pacstrap /mnt base base-devel grub intel-ucode git xorg i3 nvidia deluge mosh openssh pavucontrol scrot ffmpeg vlc xterm alsa-utils jre8-openjdk pulseaudio pulseaudio-alsa python python2 xorg-xinit dmenu nvidia-settings
 genfstab -L /mnt >> /mnt/etc/fstab
 wget raw.githubusercontent.com/InclementKing/Arch-setup/master/chrooted.sh
-wget raw.githubusercontent.com/InclementKing/Arch-setup/master/chrooted.py
 mv chrooted.sh /mnt
-mv chrooted.py /mnt
 clear
 echo base install successful- now run bash chrooted.sh
 arch-chroot /mnt
