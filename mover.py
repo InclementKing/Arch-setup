bd507b58fda1e705f9c819fb4a33ca5eb4339e0e
InclementKing <brohrsauce@gmail.com>
2018-03-27 11:06:37 -0500

    Edits

--- mover.py
+++ mover.py
@@ -3,7 +3,7 @@ import os
 files = ['bashrc', 'gitconfig', 'xinitrc', 'Xresources']
 
 for i in files:
-	os.rename(os.path.expanduser'~/arch-setup/configs/dotfiles/' + i, "/home/ben/." + i)
+	os.rename(os.path.expanduser'~/arch-setup/configs/dotfiles/' + i, os.path.expanduser'~/.' + i)
 
 os.mkdir(os.path.expanduser'~/.i3')
 os.mkdir(os.path.expanduser'~/.config/bscripts')
