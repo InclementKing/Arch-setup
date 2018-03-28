ac749e45548ef7d0b47677bad160aacc90f5b418
InclementKing <brohrsauce@gmail.com>
2018-03-28 14:53:35 -0500

    s

--- mover.py
+++ mover.py
@@ -7,4 +7,4 @@ for i in files:
 	os.renames(configs + 'dotfiles/' + i, os.path.expanduser('~/.' + i))
 
 os.renames(configs + 'i3config', os.path.expanduser('~/.i3/config'))
-os.renames(configs + 'spotify.sh'), os.path.expanduser('~/.config/bscripts/spotify.sh'))
+os.renames(configs + 'spotify.sh', os.path.expanduser('~/.config/bscripts/spotify.sh'))
