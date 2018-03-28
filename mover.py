822d58af9e91e5d7cd36ba0177f778a98b4b6889
InclementKing <brohrsauce@gmail.com>
2018-03-28 14:44:21 -0500

    s

--- mover.py
+++ mover.py
@@ -3,9 +3,9 @@ import os
 files = ['bashrc',  'xinitrc', 'Xresources']
 
 for i in files:
-	os.rename(os.path.expanduser'~/arch-setup/configs/dotfiles/' + i, os.path.expanduser'~/.' + i)
+	os.rename(os.path.expanduser('~/arch-setup/configs/dotfiles/') + i, os.path.expanduser('~/.' + i))
 
-os.mkdir(os.path.expanduser'~/.i3')
-os.mkdir(os.path.expanduser'~/.config/bscripts')
-os.rename(os.path.expanduser'~/arch-setup/configs/i3config', os.path.expanduser'~/.i3/config')
-os.rename(os.path.expanduser'~/.config/bscripts/spotify.sh)
+os.mkdir(os.path.expanduser('~/.i3'))
+os.mkdir(os.path.expanduser('~/.config/bscripts'))
+os.rename(os.path.expanduser('~/arch-setup/configs/i3config', os.path.expanduser('~/.i3/config'))
+os.rename(os.path.expanduser('~/.config/bscripts/spotify.sh'))
