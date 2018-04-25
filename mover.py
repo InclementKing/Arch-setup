277be29aa06936a49ed5edd90ce6a1cae13d931c
InclementKing <brohrsauce@gmail.com>
2018-04-25 12:27:25 -0500

    hittin that whip

--- mover.py
+++ mover.py
@@ -1,10 +1,12 @@
 import os
 
-configs = os.path.expanduser('~/archSetup/configs/')
-files = ['bashrc',  'xinitrc', 'Xresources']
+home = os.expanduser("~")
+configs = home + '/archSetup/configs/'
+dotfiles = configs + "dotfiles/"
+files = os.listdir(dotfiles)
 
-for i in files:
-	os.renames(configs + 'dotfiles/' + i, os.path.expanduser('~/.' + i))
+for file in files:
+	os.renames(dotfiles + file, home + '/.' + file)
 
-os.renames(configs + 'i3config', os.path.expanduser('~/.i3/config'))
-os.renames(configs + 'spotify.sh', os.path.expanduser('~/.config/bscripts/spotify.sh'))
+os.renames(configs + 'i3config', home + '/.i3/config')
+os.renames(configs + 'spotify.sh', home + '/.config/bscripts/spotify.sh')
