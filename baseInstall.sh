15bdbb105e579106838495c761b5a4e0d6b95602
InclementKing <37747224+InclementKing@users.noreply.github.com>
2018-04-25 11:56:41 -0500

    Update baseInstall.sh

--- baseInstall.sh
+++ baseInstall.sh
@@ -3,7 +3,7 @@ parted /dev/sda mklabel msdos
 parted -a optimal /dev/sda mkpart primary 0% 100%
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
-pacstrap /mnt base base-devel grub intel-ucode playerctl git xorg i3 nvidia deluge mosh openssh pavucontrol telegram-desktop scrot ffmpeg vlc xterm alsa-utils jre8-openjdk pulseaudio pulseaudio-alsa python python2 xorg-xinit dmenu nvidia-settings
+pacstrap /mnt base base-devel grub intel-ucode rxvt-unicode playerctl git xorg i3 nvidia deluge mosh openssh pavucontrol telegram-desktop scrot ffmpeg vlc xterm alsa-utils jre8-openjdk pulseaudio pulseaudio-alsa python python2 xorg-xinit dmenu nvidia-settings
 genfstab -L /mnt >> /mnt/etc/fstab
 wget raw.githubusercontent.com/InclementKing/archSetup/master/chrooted.sh
 mv chrooted.sh /mnt
